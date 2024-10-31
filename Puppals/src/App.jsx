24b816dfd9520cb65f5ccb07f28298ce8d070161
Kelsco <kka245@gmail.com>
2024-10-30 22:41:08 -0400

    WIP

--- Puppals/src/App.jsx
+++ Puppals/src/App.jsx
@@ -5,20 +5,40 @@ import { useState } from 'react'
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
 
- console.log("puppyList: ", puppyList);
+  // console.log("puppyList: ", puppyList);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup);
+
+  
 
   return (
     <>
-  <div className="App">  <h2>Congratulations!!</h2>
-  
-  {puppies.map((puppy) => {
-    return <p key={puppy.id}>{puppy.name}</p>;
-  })
-    } 
-   
-  </div>
-  </>
+      <div className="App">  <h2>Cutie Patooties!!</h2>
+
+        {puppies.map((puppy) => {
+          return <p onClick={() => {
+            setFeatPupId(puppy.id)
+          }} key={puppy.id}>{puppy.name}</p>;
+        })
+        }
+       {featPupId && <p>{ featPupId }</p>}
+
+       {featPupId && (
+    <div>
+      <h2>{featuredPup.name}</h2>
+      <ul>
+        <li>Age: {featuredPup.age}</li>
+        <li>Email: {featuredPup.email}</li>
+      </ul>
+    </div>
+  )
+
+  }
+
+      </div>
+    </>
   );
 }
 
